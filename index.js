31f5d530f6b42a9671f61d1ce60900ae1b85071f
David Fekke <david@fekke.com>
2021-12-19 22:23:45 -0500

    Adding Internet Button support

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import changeState, { redState, blueState, greenState, offState } from './changestate.js';
 import currentMetar from './currentMetar.js';
+import { particleButtonStream, particleColorForWeather } from './internetbutton.js';
 
-const main = async () => {
+const setCurrentWX = async () => {
     const metar = await currentMetar();
+    particleColorForWeather(metar.flight_category);
 
     switch (metar.flight_category) {
         case 'VFR':
@@ -20,6 +22,21 @@ const main = async () => {
         default:
             changeState(offState);
     }
+}
+
+//const stream = await particle.getEventStream({ deviceId: device, auth: access_token});
+particleButtonStream.on('event', function(data) {
+    if (data.name === 'readyforwx') {
+        console.log("Watching for event: ", data);
+        setCurrentWX();
+    }
+    if (data.name === 'pushcolor') {
+        console.log("Color was pushed: ", data);
+    }
+});
+
+const main = async () => {
+    setCurrentWX();
 }
 
 // Starting the light show
---
+++ internetbutton.js
@@ -0,0 +1,41 @@
+import Particle from 'particle-api-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const particle = new Particle();
+const access_token = process.env.particle_access_token;
+const device = process.env.button_device;
+const vfrColor = [0,255,0];
+const mvfrColor = [0,0,255];
+const ifrColor = [255,0,0];
+const lifrColor = [255,0,255];
+
+const getWeatherColor = (wxFlyingRules) => {
+    if (wxFlyingRules === 'VFR') {
+        return vfrColor;
+    } else if (wxFlyingRules === 'MVFR') {
+        return mvfrColor;
+    } else if (wxFlyingRules === 'LIFR') {
+        return lifrColor;
+    } else { // if IFR
+        return ifrColor;
+    }
+}
+
+const particleColorForWeather = async (wxFlyingRules) => {
+    let weatherColor = getWeatherColor(wxFlyingRules);
+    console.log('colorForWeather called');
+    console.log(`Weather color is ${weatherColor}`);
+    const publishEventPr = particle.publishEvent({ name: 'pushcolor', data: weatherColor.join(','), isPrivate: true, auth: access_token }); // isPrivate: true,
+    try {
+        const data = await publishEventPr;
+        if (data.body.ok) { console.log("Event published succesfully") }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const particleButtonStream = await particle.getEventStream({ deviceId: device, auth: access_token});
+
+export { particleButtonStream, particleColorForWeather };
