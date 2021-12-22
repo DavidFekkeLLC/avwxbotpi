bc3172a573d63feb9bce8dbe4ceb88bc291fa99c
David Fekke <david@fekke.com>
2021-12-22 09:10:25 -0500

    Adding shell script change

--- resetavwxbot.sh
+++ resetavwxbot.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-pm2 stop avwxbotpi
-pm2 delete avwxbotpi
+pm2 stop avwxbot
+pm2 delete avwxbot
 pm2 save
 rm -rf ~/apps/avwxbot
 mkdir ~/apps/avwxbot
-cp -r ~/actions-runner/_work/avwxbotpi/avwxbotpi/* ~/Documents/projects/avwxbot
+cp -r ~/actions-runner/_work/avwxbotpi/avwxbotpi/* ~/apps/avwxbot
 cd ~/apps/avwxbot
-pm2 start index.js --name avwxbotpi
+pm2 start index.js --name avwxbot
 pm2 save
